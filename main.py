f3d79a736d873c5a9be1d0f75b0b66279b105a1d
Mehrab Hossain <mehrab.cse16@gmail.com>
2022-10-10 11:59:35 +0600

    Login authentication

--- main.py
+++ main.py
@@ -14,38 +14,37 @@ class LoginScreen(Screen):
 
     def validation(self):
         users = pd.read_csv('users.csv', sep=',')
-        print(self.email.text)
-        if self.email.text != '' and self.password.text != 0:
+        if self.email.text != '' and self.password.text != '':
             # convert the numpy arry into a list to access the index easily ;p 
             temp_email = users['Email'].unique().tolist()
             temp_password = users['Password'].unique().tolist()
             if self.email.text in temp_email and self.password.text in temp_password:
                 if temp_email.index(self.email.text) == temp_password.index(self.password.text):
                     print("Login Successfull")
-                    app.root.manger.current = 'app'
+                    self.parent.current = "app"
                     self.email.text = ""
                     self.password.text = ""
 
-    #     else:
-    #         show_alert_dialog()
+            else:
+                n = self.parent.get_screen("login")
+                msg = self.email.text
+                n.show_alert_dialog(msg)
+        else:
+            n = self.parent.get_screen("login")
+            msg = ''
+            n.show_alert_dialog(msg)
 
-    # def show_alert_dialog(self):
-    #     self.dialog = MDDialog(
-    #         text="Discard draft?",
-    #         buttons=[
-    #             MDFlatButton(
-    #                 text="CANCEL",
-    #                 theme_text_color="Custom",
-    #                 text_color=self.theme_cls.primary_color,
-    #             ),
-    #             MDFlatButton(
-    #                 text="DISCARD",
-    #                 theme_text_color="Custom",
-    #                 text_color=self.theme_cls.primary_color,
-    #             ),
-    #         ],
-    #     )
-    #     self.dialog.open()
+    def show_alert_dialog(self, msg):
+        if msg=="":
+            self.dialog = MDDialog(
+                text="Enter the email and password correctly",
+            )
+        else:
+            self.dialog = MDDialog(
+                text= msg + " doesn't exits",
+            )
+        self.dialog.open()
+        
 
 class SignupScreen(Screen):
     pass 
@@ -57,13 +56,13 @@ class BloodBank(MDApp):
 
     def build(self):
         Builder.load_file("design.kv")
-        self.theme_cls.theme_style = "Dark"
+        # self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Red"
-        self.sm = ScreenManager()
-        self.sm.add_widget(LoginScreen())
-        self.sm.add_widget(SignupScreen())
-        self.sm.add_widget(AppScreen())
-        return self.sm
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen())
+        sm.add_widget(SignupScreen())
+        sm.add_widget(AppScreen())
+        return sm
 
     def login(self):
         login_screen = self.root.get_screen('login')
